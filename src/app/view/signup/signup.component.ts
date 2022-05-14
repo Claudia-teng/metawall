c623de22c47432f09510b177f46a394e8ade13f4
Claudia-teng <sydeng102@gmail.com>
2022-05-14 16:29:21 +0800

    add signup api

--- src/app/view/signup/signup.component.ts
+++ src/app/view/signup/signup.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { SignupUser } from 'src/app/model';
+import { AuthService } from 'src/app/service';
 
 @Component({
   selector: 'signup',
@@ -7,17 +10,49 @@ import { Router } from '@angular/router';
   styleUrls: ['./signup.component.sass']
 })
 export class SignupComponent {
+  public signupForm: FormGroup;
   public loading: boolean;
+  public confirmError: boolean;
   public signupFailed: boolean;
 
-  constructor(private router: Router) {}
+  constructor(private authService: AuthService,
+              private formBuilder: FormBuilder,
+              private router: Router) {}
+
+  ngOnInit() {
+    this.signupForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.required, Validators.email] ],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      confirmPassword: ['', [Validators.required, Validators.minLength(8)]],
+    }, {validators: this.checkPasswords})
+  }            
 
   public onSignup(): void {
     this.loading = true;
-    setTimeout(() => {
-      this.loading = false;
-      // this.signupFailed = true;
-      this.router.navigate(['/pages/news-feed']);
-    }, 3000);
+    const signupUser: SignupUser  = {
+      name: this.signupForm.value.name,
+      email: this.signupForm.value.email,
+      password: this.signupForm.value.password,
+      confirmPassword: this.signupForm.value.confirmPassword
+    }
+    this.authService.signup(signupUser).subscribe({
+      next: res => {
+        this.loading = false;
+        localStorage.setItem('jwt', 'Bearer ' + res.token);
+        this.router.navigate(['/pages/news-feed']);
+      },
+      error: err => {
+        this.loading = false;
+        this.signupFailed = true;
+      }
+    })
+  }
+
+  private checkPasswords: ValidatorFn = (group: AbstractControl):  ValidationErrors | null => { 
+    let pass = group.get('password')?.value;
+    let confirmPass = group.get('confirmPassword')?.value
+  
+    return pass === confirmPass ? null : { notSame: true }
   }
 }
